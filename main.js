e71a03c84de9c1fccef263faff4cdb83baf6147e
Thomas Freeman <tomclaudefreeman@gmail.com>
2024-04-01 15:25:37 +1100

    Add keyboard navigation to search bar suggestions

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+const suggestionCount = 6;
+let currentSelectedSuggestion = -1;
+
 let searchbarElement = document.getElementById('searchbar');
 
-searchbarElement.addEventListener('input', function(event) {
+searchbarElement.addEventListener('input', event => {
     let youtubeScriptElement = document.getElementById('youtubeScript');
     let parentElement = youtubeScriptElement.parentElement;
 
@@ -12,24 +15,48 @@ searchbarElement.addEventListener('input', function(event) {
     parentElement.appendChild(youtubeScriptElement);
 });
 
-searchbarElement.addEventListener('keypress', function(event) {
-    if(event.key !== 'Enter') return;
-    window.location.href = searchResultsURLForQuery(searchbarElement.value);
+searchbarElement.addEventListener('keydown', event => {
+    let suggestionsElement = document.getElementById('suggestions');
+
+    if(event.key === 'Enter') {
+        if(currentSelectedSuggestion === -1) {
+            window.location.href = searchResultsURLForQuery(searchbarElement.value);
+        } else {
+            let selectedSuggestionElement = Array.from(suggestionsElement.children)[currentSelectedSuggestion];
+            window.location.href = searchResultsURLForQuery(selectedSuggestionElement.textContent);
+        }
+    } else if(event.key === 'ArrowUp') {
+        event.preventDefault();
+        currentSelectedSuggestion--;
+        if(currentSelectedSuggestion <= -2) {
+            currentSelectedSuggestion = suggestionCount - 1;
+        }
+        updateItemElementClasses(suggestionsElement);
+    } else if(event.key === 'ArrowDown') {
+        event.preventDefault();
+        currentSelectedSuggestion++;
+        if(currentSelectedSuggestion >= suggestionCount) {
+            currentSelectedSuggestion = -1;
+        }
+        updateItemElementClasses(suggestionsElement);
+    }
+
+    console.log(currentSelectedSuggestion);
 });
 
-let suggestionsURLForQuery = function(query) {
+let suggestionsURLForQuery = query => {
     let encoded = encodeURIComponent(query);
     return `https://suggestqueries-clients6.youtube.com/complete/search?client=youtube&ds=yt&q=${encoded}`;
 };
 
-let searchResultsURLForQuery = function(query) {
+let searchResultsURLForQuery = query => {
     let encoded = encodeURIComponent(query);
     return `https://youtube.com/results?search_query=${encoded}`;
 };
 
 window.google = {};
 window.google.ac = {};
-window.google.ac.h = function(rawResponse) {
+window.google.ac.h = rawResponse => {
     let parsedResponse = parseYouTubeAPIResponse(rawResponse)
 
     let interfaceElement = document.getElementById('interface');
@@ -42,19 +69,27 @@ window.google.ac.h = function(rawResponse) {
     suggestionsElement.id = 'suggestions';
     interfaceElement.appendChild(suggestionsElement);
 
-    parsedResponse.suggestions.forEach(suggestion => {
-        let itemElement = document.createElement('li');
-        let linkElement = document.createElement('a');
-        linkElement.textContent = suggestion;
-        linkElement.href = searchResultsURLForQuery(suggestion);
-        itemElement.appendChild(linkElement);
-        suggestionsElement.appendChild(itemElement);
-    });
-}
+    for(let [index, suggestion] of parsedResponse.suggestions.entries()) {
+        suggestionsElement.appendChild(itemElementForSuggestion(suggestion, index));
+    };
+};
 
-let parseYouTubeAPIResponse = function(response) {
+let parseYouTubeAPIResponse = response => {
     let [query, suggestionsData] = response;
-    let suggestions = suggestionsData.map(suggestion => suggestion[0]).slice(0, 6);
+    let suggestions = suggestionsData.map(suggestion => suggestion[0]).slice(0, suggestionCount);
 
     return { query, suggestions };
+};
+
+let itemElementForSuggestion = (suggestion, index) => {
+    let itemElement = document.createElement('li');
+    itemElement.textContent = suggestion;
+
+    return itemElement;
+};
+
+let updateItemElementClasses = suggestionsElement => {
+    for(let [index, itemElement] of Array.from(suggestionsElement.children).entries()) {
+        itemElement.className = (index == currentSelectedSuggestion) ? 'selected' : '';
+    }
 };
